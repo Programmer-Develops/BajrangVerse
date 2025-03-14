914dd244382f932f8c88a0c97b1b2bf9090284f6
Programmer-Develops <shachipandya29@gmail.com>
2025-03-14 16:23:45 +0530

    --

--- bajrang-verse/src/App.jsx
+++ bajrang-verse/src/App.jsx
@@ -7,23 +7,13 @@ function App() {
 
   return (
     <>
-      <div className="logo-div">
-        <a href="" target="_blank">
-          <img src={BajrangV} className="logo" alt="Bajrang Verse logo" />
-        </a>
+      <div class = "container">
+        <div className="logo-div">
+          <a href="" target="_blank">
+            <img src={BajrangV} className="logo" alt="Bajrang Verse logo" />
+          </a>       
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
