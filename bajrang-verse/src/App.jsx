2caf36005ab0b3129d8b672bf7df7c786f16eb2a
Programmer-Develops <shachipandya29@gmail.com>
2025-03-23 22:11:28 +0530

    setting up project...

--- bajrang-verse/src/App.jsx
+++ bajrang-verse/src/App.jsx
@@ -13,6 +13,20 @@ function App() {
             <img src={BajrangV} className="logo" alt="Bajrang Verse logo" />
           </a>       
         </div>
+        {/* division for chat bot area in which there will be text box of user and send button aside in a box. */}
+        <div className="chat-box">
+          <div className="chat-area">
+            {/* <div className="chat-msg">
+              <div className="msg">
+                <p>Hi! I am Bajrang, your personal assistant. How can I help you today?</p>
+              </div>
+            </div> */}
+          </div>
+          <div className="chat-input">
+            <input type="text" placeholder="Type a message..." />
+            <button>Send</button>
+          </div>
+        </div>
       </div>
     </>
   )
